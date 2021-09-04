1a580ff979ff9ee69f8e67f838ddb5bed7aa30c5
Neplasma <38712320+Neplasma@users.noreply.github.com>
2021-09-04 22:06:32 +0930

    Add files via upload
    Add chapter 01 practice 01

---
+++ c01-p01 hello.py
@@ -0,0 +1,12 @@
+#Chapter 01 Practice 01
+#This program says hello and akss for my name
+
+print('Hello World!')
+print('What is your name?')
+myName = input()
+print('It is good to meet you, '+myName)
+print('The length of your name is: ')
+print(len(myName))
+print('What is your age?')
+myAge=input()
+print('You will be ' + str(int(myAge)+1)+' in a year.')
